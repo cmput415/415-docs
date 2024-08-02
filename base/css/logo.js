c240d2567fd1609f96ab24c8815ffd0f091f2076
Justin Meimar <meimar@ualberta.ca>
2024-08-01 18:28:07 -0600

    add other logos and team number quantities

--- base/css/logo.js
+++ base/css/logo.js
@@ -2,15 +2,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     let assignmentLogo = document.getElementById('assignment-logo');
-
+    let playersImage = document.getElementById('players-img');
+    console.log(playersImage);
 
     // Function to add hover effect
     function addHoverEffect(section) {
         section.addEventListener("mouseenter", function() {
             const sectionId = section.id;
+            
+            // Replace the assignment logo with the one currently being hovered over. 
             const currentSrc = assignmentLogo.src;
             const newSrc = currentSrc.replace(/[^\/]+(?=-logo\.png$)/, sectionId);
             assignmentLogo.src = newSrc;
+ 
+            // Replace the player count for the assignment with current hover
+            const currentPlayerSrc = playersImage.src;
+            if (sectionId === "gazprea") {
+                playersImage.src = playersImage.src.replace(/[^\/]+(?=\.png$)/, "players-4");
+            } else if (sectionId === "vcalc") {
+                playersImage.src = playersImage.src.replace(/[^\/]+(?=\.png$)/, "players-2");
+            } else if (sectionId == "scalc" || sectionId == "generator") {
+                playersImage.src = playersImage.src.replace(/[^\/]+(?=\.png$)/, "players-1");
+            } else {
+                playersImage.src = playersImage.src.replace(/[^\/]+(?=\.png$)/, "empty");;
+            }
+
         }); 
         
         section.addEventListener("mouseleave", function() {
